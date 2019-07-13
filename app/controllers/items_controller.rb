7790cd6914b7ddbb49da7f1d475cc53fa2a1b56f
Numakura <kazuma@example.comnew.pillow26@gmail.com>
2019-07-13 16:48:07 +0900

    登録機能完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,26 @@
 class ItemsController < ApplicationController
   def show
-    @item = Item.find (params[:id])
+    @item = Item.find(params[:id])
   end
 
   def index
     @items = Item.all
   end
 
+  def new
+    @item = Item.new
+  end
 
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to @item
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price,:seller,:description,:email,:image_url)
+  end
 end
