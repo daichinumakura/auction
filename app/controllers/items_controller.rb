cef1e6ed079a4e436b5fd53f61b9b731b682d0ab
Numakura <kazuma@example.comnew.pillow26@gmail.com>
2019-07-13 17:18:22 +0900

    更新機能完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,15 @@ class ItemsController < ApplicationController
     redirect_to @item
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+  @item = Item.find(params[:id])
+  @item.update(item_params)
+  redirect_to @item
+  end
   private
 
   def item_params
